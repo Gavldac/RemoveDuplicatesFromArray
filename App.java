aacf06ce1417b4a7d4000ad2034128d260f00764
Gavldac <75696834+Gavldac@users.noreply.github.com>
2024-10-29 18:17:53 -0600

    Update App.java - included better documentation regarding the challenge

--- App.java
+++ App.java
@@ -1,5 +1,10 @@
 import java.util.Arrays;
 
+/**
+ * @author LeetCode challenge 26.
+ * @author Test client by Edwin Casady
+ * @author Solution by
+ */
 public class App {
 
     /*
@@ -15,32 +20,33 @@ public class App {
      * return k (the number of unique elements)
      */
 
+    /**
+     * Removes duplicated values from an int[] and returns the number k of unique elements.
+     * Note: the array is modified to preserve order of a sorted array but will not be resized.
+     * The array will likely have corrupt data after the last unique value. 
+     * @param nums []
+     * @return int k - the number of unique elements
+     */
+
     private static int removeDuplicates(int[] nums) {
         return 0; // TODO
     }
 
-
-    
-/////////////////////////////TEST CLIENT///////////////////////////////////////
+    ///////////////////////////// TEST CLIENT///////////////////////////////////////
 
     public static void main(String[] args) throws Exception {
 
-        int [] test1 = {1,1,2};
+        int[] test1 = { 1, 1, 2 };
         int k1 = removeDuplicates(test1);
         System.out.println("test1");
         System.out.println("Expected: [1, 2, _] k: " + 2);
         System.out.println("Results:  " + Arrays.toString(test1) + " k: " + k1);
-        
-        int [] test2 = {0,0,1,1,1,2,2,3,3,4};
+
+        int[] test2 = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
         int k2 = removeDuplicates(test2);
         System.out.println("test2");
         System.out.println("Expected: [0, 1, 2, 3, 4, _, _, _, _, _] k: " + 5);
         System.out.println("Results:  " + Arrays.toString(test2) + " k: " + k2);
-        
-    
-
-
-
 
     }
 }
